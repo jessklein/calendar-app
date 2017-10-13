addc022b5d381ebab99c2cdcceded2a719174372
Jessica Klein <jess.klein@live.com>
2017-10-13 18:35:19 -0400

    Initial app push

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'calendar/show'
+ resource :calendar, only: [:show], controller: :calendar
+  root to: "calendar#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
